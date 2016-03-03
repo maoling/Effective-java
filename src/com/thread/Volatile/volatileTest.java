e58fc4b903278117ffb65ee005b9add7796cdd14
maoling <maoling199210191@sina.com>
2016-03-03 18:06:21 +0800

    18:06

--- src/com/thread/Volatile/volatileTest.java
+++ src/com/thread/Volatile/volatileTest.java
@@ -1,16 +1,38 @@
 package com.thread.Volatile;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /* http://www.importnew.com/18126.html
  http://www.importnew.com/17820.html
  http://blog.jobbole.com/98028/
  */
 public class volatileTest {
 
-	public volatile int inc = 0;
+	public int inc = 0;
+	Lock lock = new ReentrantLock();
 
 	public void increase() {
-		inc++;
+		lock.lock();
+		try {
+			inc++;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
 	}
+
+	/*
+	 * public synchronized void increase() { inc++; }
+	 */
+
+	/*
+	 * public volatile AtomicInteger inc = new AtomicInteger();
+	 * 
+	 * public void increase() { inc.addAndGet(1); }
+	 */
 
 	public static void main(String[] args) {
 		final volatileTest test = new volatileTest();
