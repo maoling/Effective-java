ad90a548481b99f922b09a6b9362a49a4682ee1b
maoling <maoling199210191@sina.com>
2016-03-07 00:45:57 +0800

    0;45

--- src/com/util/StringTest.java
+++ src/com/util/StringTest.java
@@ -1,5 +1,9 @@
 package com.util;
-
+/**
+ * http://www.importnew.com/18167.html
+ * javap是什么？网上有人称之为 反汇编器，可以查看java编译器为我们生成的字节码。通过它，我们可以对照源代码和字节码，从而了解很多编译器内部的工作。
+ *
+ */
 public class StringTest {
 
 	/**
@@ -7,7 +11,20 @@ public class StringTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str = new String("abc");
+		int m = 0;
+		for(int i=0;i<100;i++){
+			//m = m++;//  0
+			m = ++m;  //  100
+		}
+		System.out.println(m);
+		/*int i = 2;
+		int j = 3;*/
+		
+		/*String str1 = "I";
+        str1 += "love"+"java";        //1)
+        //str1 = str1+"love"+"java";      //2)
+*/		
+		//String str = new String("abc");
 		
 		/*String a = "hello";
         String b =  new String("hello");
