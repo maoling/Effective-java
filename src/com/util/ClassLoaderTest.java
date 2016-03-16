ae6fd22a14e5b2b677d5ec02eec04d5056588db0
maoling <maoling199210191@sina.com>
2016-03-16 16:06:31 +0800

    1

---
+++ src/com/util/ClassLoaderTest.java
@@ -0,0 +1,23 @@
+package com.util;
+/*
+ * http://www.importnew.com/15362.html
+ * */
+import java.net.URL;
+
+public class ClassLoaderTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();  
+		for (int i = 0; i < urls.length; i++) {  
+		    System.out.println(urls[i].toExternalForm());  
+		}
+		
+		System.out.println();
+		System.out.println(System.getProperty("sun.boot.class.path"));
+		
+		
+		
+	}
+
+}
