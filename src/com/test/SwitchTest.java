902ad9e4910b8fd2d62ba2d9e401a37da575f716
maoling <maoling199210191@sina.com>
2016-04-07 22:41:53 +0800

    22：41

---
+++ src/com/test/SwitchTest.java
@@ -0,0 +1,15 @@
+package com.test;
+public class SwitchTest{//1
+    public static void main(String[] args) {//2
+        System.out.println("value="+switchit(4));//3
+    }//4
+    public static String switchit(int x) {
+        //int j=1;
+    	float j = 4.0f;
+        switch (x) {
+        	
+        
+        }
+        return "";
+    }
+}
--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -1,37 +1,23 @@
 package com.test;
-public class Test
-{
-    public static int x;
-    public static void main(String[] args,String[] arg2) {
-		System.out.println("fuck");
-	}
-    
-    public static void main(String args[])
-    {
-    	int i=0;
-    	Integer j = new Integer(0);
-    	System.out.println(i==j);
-    	System.out.println(j.equals(i));
-    	
-    	/*int i = 5;
-    	int j = 10;
-    	System.out.println(i + ~j);*/
-    	
-    	/*Integer n1 = new Integer(47);
-        Integer n2 = new Integer(47);
-        System.out.print(n1 == n2);
-        System.out.print(",");
-        System.out.println(n1 != n2);*/
-    	
-        /*Integer a = 127;
-        Integer b = 127;
-        Integer c = 128;
-        Integer d = 128;
-        System.out.println(a==b);
-        System.out.println(c==d);*/
-    	//System. out. println("Value is" + x);
-    	/*double d1=-0.5;
-    	System.out.println("Ceil d1="+Math.ceil(d1));*/
-    	//System.out.println("floor d1="+Math.floor(d1));
-   }
+public class Test {
+    private static int j = 0;
+ 
+    private static Boolean methodB(int k) {
+        j += k;
+        return true;
+    }
+ 
+    public static void methodA(int i) {
+        boolean b;
+        b = i < 10 | methodB(4);
+        b = i < 10 || methodB(8);
+ 
+    }
+ 
+    public static void main(String args[]) {
+    	char Ï²»¶='ÖÐ';
+    	System.out.println(Ï²»¶);
+        /*methodA(0);
+        System.out.println(j);*/
+    }
 }
