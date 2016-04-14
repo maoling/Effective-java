3047d7f7d2f40586e65d5dfa900c898174d01dd4
maoling <maoling199210191@sina.com>
2016-04-14 23:43:18 +0800

    23:43

--- src/com/test/Demo.java
+++ src/com/test/Demo.java
@@ -1,16 +1,24 @@
 package com.test;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-class A2{
+/*class A2{
 	public int x=0;
 	public A2(){
 	}
-}
+}*/
 public class Demo {
 	public static void main(String[] args){
-       A2 a=new A2();
+		List  Listlist1 = new ArrayList();
+	    Listlist1.add(0);
+	    List Listlist2 = Listlist1;
+	    System.out.println(Listlist1.get(0) instanceof Integer);
+	    System.out.println(Listlist2.get(0) instanceof Integer);
+		
+      /* A2 a=new A2();
        final A2 s=a;
        System.out.println(s.x);
        a.x++;
-       System.out.println(s.x);
+       System.out.println(s.x);*/
     }
 }
--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,7 +3,10 @@ public class Test
 {
     public static void main(String[] args)
     {
-        int x = 0;
+    	int k = f_test();
+        System.out.println(k);
+    	
+    	/*int x = 0;
         int y = 0;
         int k = 0;
         for (int z = 0; z < 5; z++) {
@@ -14,6 +17,19 @@ public class Test
                 k++;
             }
         }
-        System.out.println(x +" "+y +" "+k);
+        System.out.println(x +" "+y +" "+k);*/
+    }
+    
+    public static int f_test(){
+        int a = 0;
+        try{
+            a = 1;
+            return a;
+        }
+        finally{
+            System.out.println("It is in final chunk.");
+            a = 2;
+            return a;
+        }
     }
 }
